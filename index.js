a85640be19df411ba921f5ff06c0a62f82926b2f
Harold Nwosu <harold.nwosu@gmail.com>
2023-12-04 22:59:47 -0600

    MongoDB database integration

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
+require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
+const Note = require('./models/note');
 
 const app = express();
 
@@ -19,24 +21,6 @@ morgan.token('note', (request, response) => {
 
 app.use(morgan(':method :url :res[content-length] :response-time ms :note'));
 
-let notes = [
-  {
-    id: 1,
-    content: "HTML is easy",
-    important: true
-  },
-  {
-    id: 2,
-    content: "Browser can execute only JavaScript",
-    important: false
-  },
-  {
-    id: 3,
-    content: "GET and POST are the most important methods of HTTP protocol",
-    important: true
-  }
-]
-
 const generateId = () => {
     const maxId = notes.length > 0
         ? Math.max(...notes.map(note => note.id))
@@ -50,47 +34,69 @@ app.get('/', (request,response) => {
 })
 
 app.get('/api/notes', (request,response) => {
-    response.json(notes);
+    Note.find({}).then(notes => {
+        response.json(notes);
+    })
 })
 
 app.get('/api/notes/:id', (request, response) => {
-    const id = Number(request.params.id);
-    const note = notes.find(note => note.id === id);
-
-    if (note) {
-        response.json(note);
-    } else {
-        response.status(404).end();
-    }
+    Note.findById(request.params.id)
+        .then(note => {
+            response.json(note);
+        }) 
 })
 
-app.post('/api/notes', (request, response) => {
+app.post('/api/notes', (request, response, next) => {
     const body = request.body;
-    
-    if (!body.content) {
-        return response.status(400).json({
-            error: "content missing"
-        });
-    }
+
+    const note = new Note({
+        content: body.content,
+        important: body.important || false,
+    });
+
+    note.save()
+        .then(savedNote => {
+            response.json(savedNote);
+        })
+        .catch(error => next(error))
+})
+
+app.put('/api/notes/:id', (request, response, next) => {
+    const body = request.body;
 
     const note = {
         content: body.content,
-        important: body.important || false,
-        id: generateId(),
+        important: body.important,
     }
 
-    notes = notes.concat(note)
-    response.json(note);
+    Note.findByIdAndUpdate(request.params.id, note, { new: true, runValidators: true, context: 'query'})
+        .then(updatedNote => {
+            response.json(updatedNote)
+        })
+        .catch(error => next(error))
 })
 
-app.delete('/api/notes/:id', (request, response) => {
-    const id = Number(request.params.id);
-    notes = notes.filter(note => note.id !== id);
-    
-    response.status(204).end();
+app.delete('/api/notes/:id', (request, response, next) => {
+    Note.findByIdAndDelete(request.params.id)
+        .then(result => {
+            response.status(204).end();
+        })
+        .catch(error => next(error))
 })
 
-const PORT = process.env.PORT || 3001
+const errorHandler = (error, request, response, next) => {
+    console.log(error.message);
+    if (error.name === 'CastError') {
+        return response.status(400).send({ error: 'malformatted id'});
+    } else if (error.name == 'ValidationError') {
+        return response.status(400).send({ error: error.message })
+    }
+    next(error);
+}
+
+app.use(errorHandler);
+
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
---
+++ mongo.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length<3) {
+  console.log('give password as argument')
+  process.exit(1)
+}
+
+const password = process.argv[2]
+
+const url =
+  `mongodb+srv://haroldnwosu:${password}@cluster0.hhbc8xk.mongodb.net/noteApp?retryWrites=true&w=majority`
+
+mongoose.set('strictQuery',false)
+mongoose.connect(url)
+
+const noteSchema = new mongoose.Schema({
+  content: String,
+  important: Boolean,
+})
+
+const Note = mongoose.model('Note', noteSchema)
+
+// const note = new Note({
+//   content: 'Mongo is the database',
+//   important: true,
+// })
+
+// note.save().then(result => {
+//     console.log('note saved!')
+//     console.log(result)
+//     mongoose.connection.close()
+// })
+
+Note.find({}).then(result => {
+    result.forEach(note => {
+        console.log(note)
+    })
+    mongoose.connection.close()
+})
